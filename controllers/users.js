ee83c58800d0dfeb47b4c558d7ef21d5f17a4abf
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-04-17 23:47:33 +0400

    fixed: исправить контроллер обновления профиля

--- controllers/users.js
+++ controllers/users.js
@@ -66,29 +66,38 @@ const login = (req, res, next) => {
 
 const updateProfile = (req, res, next) => {
   const { email, name } = req.body;
+  User.findOne({ email }, (queryError) => {
+    if (queryError) {
+      if (queryError.code === 11000 || queryError.code === 11001) {
+        const error = new ConflictError('Пользователь с таким email уже существует');
+        return next(error);
+      }
+      return next(queryError);
+    }
 
-  User.findByIdAndUpdate(
-    req.user._id,
-    { email, name },
-    {
-      new: true,
-      runValidators: true,
-    },
-  )
-    .then((user) => {
-      if (!user) {
-        const error = new NotFound('Такого пользователя не существует');
-        return next(error);
-      }
-      return res.status(OK).send({ data: user });
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError' || err.name === 'CastError') {
-        const error = new BadRequest('Некорректный запрос');
-        return next(error);
-      }
-      return next(err);
-    });
+    return User.findByIdAndUpdate(
+      req.user._id,
+      { email, name },
+      {
+        new: true,
+        runValidators: true,
+      },
+    )
+      .then((user) => {
+        if (!user) {
+          const error = new NotFound('Такого пользователя не существует');
+          return next(error);
+        }
+        return res.status(OK).send({ data: user });
+      })
+      .catch((err) => {
+        if (err.name === 'ValidationError' || err.name === 'CastError') {
+          const error = new BadRequest('Некорректный запрос');
+          return next(error);
+        }
+        return next(err);
+      });
+  });
 };
 
 module.exports = {
