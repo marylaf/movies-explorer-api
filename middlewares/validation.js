2a96e18116c5fcb17e118f8fd60a7b995a54e23c
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-04-18 17:31:20 +0400

    fixed: сделать name обязательным

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -10,7 +10,7 @@ const validateURL = (value) => {
 
 const validateCreateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
 
