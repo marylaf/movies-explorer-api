f4efed6b2dcca603c2d0cfc718ffbfb436219ca3
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-07-15 03:06:59 +0600

    feature: добавить разрешение cors

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,25 @@ const app = express();
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  const { method } = req;
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.status(200).end();
+  }
+  return next();
+});
+
 router.post('/signup', validateCreateUser, createUser);
 
 router.post('/signin', validateLogin, login);
