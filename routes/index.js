2a6e05016784b61257fe3e0c90ebeeb232881769
Maria Lafutkina <lafutkina.masha@yandex.ru>
2023-07-15 22:02:23 +0600

    feature: изменить роуты

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const router = require('express').Router();
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 const { validateCreateUser, validateLogin } = require('../middlewares/validation');
@@ -13,36 +12,17 @@ const app = express();
 
 app.use(cors());
 
+app.post('/signup', validateCreateUser, createUser);
+
+app.post('/signin', validateLogin, login);
+
+app.use('/users/me', auth, userRouter);
+app.use('/movies', auth, movieRouter);
+
+app.use(auth);
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-
-  const { method } = req;
-
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.status(200).end();
-  }
-  return next();
-});
-
-router.post('/signup', validateCreateUser, createUser);
-
-router.post('/signin', validateLogin, login);
-
-router.use('/', auth, userRouter);
-router.use('/', auth, movieRouter);
-
-router.use(auth);
-router.use((req, res, next) => {
   const error = new NotFound(notFoundMessage);
   return next(error);
 });
 
-module.exports = router;
+module.exports = app;
--- routes/movies.js
+++ routes/movies.js
@@ -4,10 +4,10 @@ const { createMovie } = require('../controllers/movies');
 const { deleteMovie } = require('../controllers/movies');
 const { validateCreateMovie, validateDeleteMovie } = require('../middlewares/validation');
 
-movieRouter.get('/movies', getMovies);
+movieRouter.get('/', getMovies);
 
-movieRouter.post('/movies', validateCreateMovie, createMovie);
+movieRouter.post('/', validateCreateMovie, createMovie);
 
-movieRouter.delete('/movies/:_id', validateDeleteMovie, deleteMovie);
+movieRouter.delete('/:_id', validateDeleteMovie, deleteMovie);
 
 module.exports = movieRouter;
